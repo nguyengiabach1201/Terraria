e49ec1167587d4c7f5fa76682408044a911f0ddd
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-27 09:25:05 +0000

    Update JS

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+function setCanvasSize() {
+  const dpr = window.devicePixelRatio || 2;
+
+  const width = 256, height = 256;
+  console.log(width, height);
+
+  if (canvas.width !== width * dpr || canvas.height !== height * dpr) {
+    canvas.width = width * dpr;
+    canvas.height = height * dpr;
+  }
+
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.scale(dpr, dpr);
+}
+setCanvasSize();
