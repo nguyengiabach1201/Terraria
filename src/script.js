f3d184d5b04e07804d28e30f2c7e5df3ab7e1025
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-27 09:40:45 +0000

    Added loop

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
-function generateTerrain() {
+const worldSize = 64;
 
+function generateTerrain(frequency, seed) {
+  for (let x = 0; x < worldSize; x++) {
+    for (let y = 0; y < worldSize; y++) {
+
+    }
+  }
 }
